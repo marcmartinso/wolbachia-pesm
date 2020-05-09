682de12c38ca23e41dee58ce52be6b6ba5065657
marcmartinso <marcosamartinso@gmail.com>
2020-05-09 12:50:26 -0300

    First Commited Project from RStudio

---
+++ dsjh.R
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------------------------------------- #
+print("This file was created within RStudio") 
+print("And now it lives on GitHub")
+# ---------------------------------------------------------------------------------------------------- #
+
+# ------------------------------ DATA SCIENCE - JOHN'S HOPKINS ------------------------------ #   
+
+# wolbachia-pesm
+# Ecological data on Wolbachia presence in Neotropical Drosophilids of Serra do Mar State Park(PESM), Sao Paulo, Brazil
