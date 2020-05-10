dbdbae05df0825be706e6b12d8246f5225ed61e4
Marcos Martins <36088852+marcmartinso@users.noreply.github.com>
2020-05-10 12:34:04 -0300

    Delete dsjh.R

--- dsjh.R
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------------------------------------------------- #
-print("This file was created within RStudio") 
-print("And now it lives on GitHub")
-# ---------------------------------------------------------------------------------------------------- #
-
-# ------------------------------ DATA SCIENCE - JOHN'S HOPKINS ------------------------------ #   
-
-# wolbachia-pesm
-# Ecological data on Wolbachia presence in Neotropical Drosophilids of Serra do Mar State Park(PESM), Sao Paulo, Brazil
